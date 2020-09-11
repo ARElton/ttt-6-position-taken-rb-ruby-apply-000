50a320be7df49c96ad3aaffa57f78dfe70d15352
Amelia Elton <ameliareneeelton@gmail.com>
2020-09-11 19:31:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if (board[index] == " ") || (board[index] == "")
+  if (board[index] == " ") || (board[index] == "") || (board[indes] == nil)
     return false 
   end
 end
